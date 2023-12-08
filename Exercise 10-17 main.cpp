215a465e7dd167b67546467cb92a6876e48ead96
DMFletcher87 <64287395+DMFletcher87@users.noreply.github.com>
2023-12-07 18:52:49 -0600

    Create Exercise 10-17 main.cpp
    Instructions
    Write a program that allows two players to play the tic-tac-toe game. Your program must contain the class ticTacToe to implement a ticTacToe object. Include a 3-by-3 two-dimensional array, as a private member variable, to create the board. If needed, include additional member variables. Some of the operations on a ticTacToe object are printing the current board, getting a move, checking if a move is valid, and determining the winner after each move. Add additional operations as needed.

---
+++ Exercise 10-17 main.cpp
@@ -0,0 +1,14 @@
+//Purpose: Write a program that allows two players to play the tic-tac-toe game. Your program must contain the class ticTacToe to implement a ticTacToe object. Include a 3-by-3 two-dimensional array, as a private member variable, to create the board. If needed, include additional member variables. Some of the operations on a ticTacToe object are printing the current board, getting a move, checking if a move is valid, and determining the winner after each move. Add additional operations as needed.
+
+#include <iostream>
+#include "TicTacToe.h"
+
+using namespace std;
+
+int main() {
+  // Write your main here
+  ticTacToe game;
+  game.player();
+    
+    return 0;
+}
